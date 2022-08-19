eae4f9aacb4b4b25b009f6f56fb8de5274bcfa93
João Duarte <duarte.jpdl@gmail.com>
2022-08-18 19:06:35 -0600

    db: Implementation of repository (PASS)

--- src/repository/category.js
+++ src/repository/category.js
@@ -3,8 +3,7 @@ import { categoryDB } from '../.db/category.js'
 export default class CategoryRepository {
 
     static findAll() {
-        // TODO: 
-        return undefined;
+        return categoryDB;
     }
 
 }
