e099227f2ebc49e61005775ce22c30c909e3498b
João Duarte <duarte.jpdl@gmail.com>
2022-08-19 02:32:49 -0600

    rest: Path handler implementation (PASS)

--- src/api/rest/routes.js
+++ src/api/rest/routes.js
@@ -1,7 +1,16 @@
 import { serverStatusRouterV1 } from './v1/server-status/serverStatus.controller.js'
 import { categoriesRouterV1 } from './v1/categories/categories.controller.js'
 
+const unknownRoute = (_req, res, _next) => {
+    res.status(400).json({
+        status: 400,
+        data: null,
+        error: 'The endpoint does not exist.'
+    });
+}
+
 export const setupRoutes = (app, apiVersion) => {
     app.use(`${apiVersion}/server-status`, serverStatusRouterV1)
     app.use(`${apiVersion}/categories`, categoriesRouterV1)
+    app.use(unknownRoute);
 }
