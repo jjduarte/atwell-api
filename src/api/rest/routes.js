9ea749ef656282f5cb37d2fa9119564a4d99158e
João Duarte <duarte.jpdl@gmail.com>
2022-08-19 01:30:18 -0600

    rest: Implementation of categories endpoint (PASS)

--- src/api/rest/routes.js
+++ src/api/rest/routes.js
@@ -1,5 +1,7 @@
 import { serverStatusRouterV1 } from './v1/server-status/serverStatus.controller.js'
+import { categoriesRouterV1 } from './v1/categories/categories.controller.js'
 
 export const setupRoutes = (app, apiVersion) => {
     app.use(`${apiVersion}/server-status`, serverStatusRouterV1)
+    app.use(`${apiVersion}/categories`, categoriesRouterV1)
 }
